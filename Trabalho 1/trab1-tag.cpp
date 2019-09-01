27cd990ceabea4aac268b6dbd0a36963f70174b6
carolestrella <42721928+carolestrella@users.noreply.github.com>
2019-09-01 16:57:49 -0300

    questoes 1, 3 e 4 finalizadas

--- Trabalho 1/trab1-tag.cpp
+++ Trabalho 1/trab1-tag.cpp
@@ -3,6 +3,33 @@
 #include <vector>
 using namespace std;
 
+bool connects(vector<vector<int> > grafo, int a, int b){
+    for(int i=0; i < (int)grafo[a].size(); i++){
+        if(grafo[a][i] == b){ //verifica se as conexoes do vertice atual tambem sao conexoes do vertice pai
+            return true;
+        }
+    }
+    return false;
+}
+
+double coeficiente(vector<vector<int> > grafo, int vertice){
+    int triangulo = 0, tamvertice = grafo[vertice].size();
+    double coef, possiveltriangulo;
+    for(int j=0; j<tamvertice; j++){
+        for(int k=j+1; k<tamvertice; k++){
+            if(connects(grafo, grafo[vertice][j], grafo[vertice][k])){
+                triangulo++; //atualiza o numero de triangulos formados entre os vertices ligados ao grafo[vertice] atual
+            }
+        }
+    } 
+    possiveltriangulo = (tamvertice * (tamvertice-1))/2; //calcula quantidade de possvieis triangulos que podem ser formados
+    if(tamvertice <= 1)
+        coef = 0;
+    else
+        coef = triangulo/possiveltriangulo;
+
+    return coef;
+}
 
 int main(){
 	ifstream arquivo;
@@ -16,8 +43,8 @@ int main(){
         cout << "Erro ao abrir o arquivo"<<endl;
     }
 
-    //Cria vetor de tamanho 62
-    vector<int> grafo[10000];
+    //Cria vetor de tamanho 63
+    vector< vector<int> > grafo(63);
     //variavel que conta total de conexoes
     int conexoes = 0;
 
@@ -28,20 +55,29 @@ int main(){
         conexoes++;
     }
 
-    for(int v=1; v <= 62; v++){
+    int nos = (int)grafo.size()-1;
+    double coeftotal = 0;
+    
+    for(int v=1; v <= nos; v++){
         int grau = 0;
-        cout<< "Indice: " << v <<endl;
+        cout<< "Indice: " << v << endl;
         //verifica todos os vertices ligados ao indice atual
         for(auto x : grafo[v]){
             grau++;
             cout<< "->" << x;
         }
         cout<<endl;
-        cout << "Grau: " << grau<<endl;
+        cout << "Grau: " << grau << endl;
+        double coefaglomeracao = coeficiente(grafo, v);
+        cout << "Coeficiente de Aglomeração: " << coefaglomeracao <<endl;
+        coeftotal+=coefaglomeracao;
         cout<< "==========================================" <<endl;
     }
 
-    cout<< "Total de conexoes: " <<conexoes <<endl;
+    cout<<endl;
+    cout<< "Coeficiente médio de aglomeração: "<< coeftotal/nos << endl;
+    cout<< "Total de conexoes: " << conexoes << endl;
+
     arquivo.close();
  
     return 0;
